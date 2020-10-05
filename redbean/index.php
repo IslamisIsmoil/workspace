3aed69861d4771554de074562d1753f0a2f1198f
IslamisIsmoil <islamirgashev7@gmail.com>
2020-10-06 03:14:49 +0500

    first commit

---
+++ redbean/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<div class="block__item">
+		<div class="block__el"></div>
+	</div>
+	<div class="block__item">
+		<div class="block__el"></div>
+	</div>
+</body>
+</html>
